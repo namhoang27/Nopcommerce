f1b0bd88f2c81b1d3011186e6396bf2206ed902a
Nam Hoang <hoanguye@tibco.com>
2023-05-22 09:54:55 +0700

    delete trash file

--- src/test/java/starter/interfaces/NavigationPageUI.java
+++
@@ -1,8 +0,0 @@
-package starter.interfaces;
-
-public class NavigationPageUI {
-    public static final String LOGIN_LINK = "//a[@class='ico-login']";
-    public static final String LOGOUT_LINK ="//a[@class='ico-logout']";
-    public static final String REGISTER_LINK = "//a[@class='ico-register']";
-    public static final String MYACCOUNT_LINK = "//a[@class='ico-account']";
-}
