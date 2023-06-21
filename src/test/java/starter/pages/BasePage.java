b574dab497c7bafd4f1bf81ed5da789c9f64b134
Nam Hoang <hoanguye@tibco.com>
2023-06-21 22:05:10 +0700

    Refactor code

--- src/test/java/starter/pages/BasePage.java
+++ src/test/java/starter/pages/BasePage.java
@@ -5,9 +5,6 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,11 +17,8 @@ import java.util.logging.LogManager;
 
 public class BasePage extends PageObject {
     private JavascriptExecutor jsExecutor;
-    private WebDriverWait explicitWait;
     private WebElement element;
-    private List<WebElement> elements;
     private Actions action;
-    private Select select;
 
     public List<String> getList(String xPath) {
         List<String> element = new ArrayList<>();
@@ -40,51 +34,10 @@ public class BasePage extends PageObject {
         return getDriver().getPageSource();
     }
 
-    public void clickByJs(String xpath){
-        WebElement element = getDriver().findElement(By.xpath(xpath));
-        JavascriptExecutor executor = (JavascriptExecutor)getDriver();
-        executor.executeScript("arguments[0].click();", element);
-    }
-
     public String getTextAlert() {
         return getDriver().switchTo().alert().getText();
     }
 
-    public void setTextAlert(String value) {
-        getDriver().switchTo().alert().sendKeys(value);
-    }
-
-    public void switchToWindowByID(String parentID) {
-        Set<String> allWindows = getDriver().getWindowHandles();
-        for (String runWindow : allWindows) {
-            if (!runWindow.equals(parentID)) {
-                getDriver().switchTo().window(runWindow);
-                break;
-            }
-        }
-    }
-
-    public void switchToWindowByTitle(String title) {
-        Set<String> allWindows = getDriver().getWindowHandles();
-        for (String runWindows : allWindows) {
-            getDriver().switchTo().window(runWindows);
-            String currentWin = getDriver().getTitle();
-            if (currentWin.equals(title)) {
-                break;
-            }
-        }
-    }
-
-    public void closeAllWindowsWithoutParent(String parentID) {
-        Set<String> allWindows = getDriver().getWindowHandles();
-        for (String runWindows : allWindows) {
-            if (!runWindows.equals(parentID)) {
-                getDriver().switchTo().window(runWindows);
-                getDriver().close();
-            }
-        }
-        getDriver().switchTo().window(parentID);
-    }
 
     public WebElement getElement(String locator) {
        try {
@@ -102,80 +55,7 @@ public class BasePage extends PageObject {
         return By.xpath(locator);
     }
 
-    public void clickToElement(String locator) {
-        element = getElement(locator);
-        element.click();
-    }
 
-    public void clickToElement(String locator, String... values) {
-        element = getElement(getDynamicLocator(locator, values));
-        element.click();
-    }
-
-    public void sendkeyToElement(String locator, String value) {
-        element = getElement(locator);
-        element.clear();
-        element.sendKeys(value);
-    }
-
-    public void sendkeyToElement(String locator, String value, String... values) {
-        element = getElement(locator);
-        element.clear();
-        element.sendKeys(getDynamicLocator(locator, values));
-    }
-
-    public void selectItemInDropdown(String locator, String itemValue) {
-        element = getElement(locator);
-        select = new Select(element);
-        select.selectByVisibleText(itemValue);
-    }
-
-    public String getFirstSelectedTextInDropdown(String locator) {
-        element = getElement(locator);
-        select = new Select(element);
-        return select.getFirstSelectedOption().getText();
-    }
-
-    public boolean isDropdownMultiple(String locator) {
-        element = getElement(locator);
-        select = new Select(element);
-        return select.isMultiple();
-    }
-
-    public void selectItemInCustomDropdown(String parentLocator, String childItemLocator,
-                                           String expectedItem) {
-        getElement(parentLocator).click();
-        sleepInSecond(1);
-        WebDriverWait explicitWait = new WebDriverWait(getDriver(), GlobalConstants.LONG_TIMEOUT);
-        explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(getByXpath(childItemLocator)));
-
-        List<WebElement> allItems = getElements(childItemLocator);
-        for (WebElement item : allItems) {
-            if (item.getText().equals(expectedItem)) {
-                jsExecutor = (JavascriptExecutor) getDriver();
-                jsExecutor.executeScript("arguments[0].scrollIntoView(true);", item);
-                sleepInSecond(1);
-
-                item.click();
-                sleepInSecond(1);
-                break;
-            }
-        }
-    }
-
-    public void sleepInSecond(long timeout) {
-        try {
-            Thread.sleep(timeout * 1000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-    public String getElementAttribute(String locator, String attributeName) {
-        element = getElement(locator);
-        return element.getAttribute(attributeName);
-    }
 //    public String parseSpecialText(String text) {
 //        String[] items = text.trim().split(" ");
 //        StringBuilder builder = new StringBuilder();
@@ -185,107 +65,6 @@ public class BasePage extends PageObject {
 //        return builder.toString();
 //    }
 
-    public String getElementText(String locator) {
-        element = getElement(locator);
-        return element.getText().trim();
-    }
-
-    public String getElementText(String locator, String... values) {
-        element = getElement(getDynamicLocator(locator, values));
-        return element.getText();
-    }
-
-    public int countElementSize(String locator) {
-        return getElements(locator).size();
-    }
-
-    public int countElementSize(String locator, String... values) {
-        return getElements(getDynamicLocator(locator, values)).size();
-    }
-
-    public void checkToCheckBox(String locator) {
-        element = getElement(locator);
-        if (!element.isSelected()) {
-            element.click();
-        }
-    }
-
-    public void checkToCheckBox(String locator, String... values) {
-        element = getElement(getDynamicLocator(locator, values));
-        if (!element.isSelected()) {
-            element.click();
-        }
-    }
-
-    public void uncheckToCheckBox(String locator) {
-        element = getElement(locator);
-        if (element.isSelected()) {
-            element.click();
-        }
-    }
-
-    public void uncheckToCheckBox(String locator, String... values) {
-        element = getElement(getDynamicLocator(locator, values));
-        if (!element.isSelected()) {
-            element.click();
-        }
-    }
-
-    public boolean isElementDisplayed(String locator) {
-        return getElement(locator).isDisplayed();
-    }
-
-    public boolean isElementDisplayed(String locator, String... values) {
-        return getElement(getDynamicLocator(locator, values)).isDisplayed();
-    }
-
-    public boolean isElementEnabled(String locator) {
-        return getElement(locator).isEnabled();
-    }
-
-    public boolean isElementEnabled(String locator, String... values) {
-        return getElement(getDynamicLocator(locator, values)).isEnabled();
-    }
-
-    public boolean isElementSelected(String locator) {
-        return getElement(locator).isSelected();
-    }
-
-    public boolean isElementSelected(String locator, String... values) {
-        return getElement(getDynamicLocator(locator, values)).isSelected();
-    }
-
-
-    public boolean isElementUndisplayed(String locator) {
-        if (elements.size() == 0) {
-            System.out.println("Element not in DOM");
-            System.out.println("End time = " + new Date().toString());
-            return true;
-        } else if (elements.size() >= 0 && !elements.get(0).isDisplayed()) {
-            System.out.println("Element in DOM but not visible/displayed");
-            System.out.println("End time = " + new Date().toString());
-            return true;
-        } else {
-            System.out.println("Element in DOM and visible");
-            return true;
-        }
-    }
-
-    public boolean isElementUndisplayed(String locator, String... values) {
-        if (elements.size() == 0) {
-            System.out.println("Element not in DOM");
-            System.out.println("End time = " + new Date().toString());
-            return true;
-        } else if (elements.size() >= 0 && !elements.get(0).isDisplayed()) {
-            System.out.println("Element in DOM but not visible/displayed");
-            System.out.println("End time = " + new Date().toString());
-            return true;
-        } else {
-            System.out.println("Element in DOM and visible");
-            return true;
-        }
-    }
-
     public void switchToFrame(String locator) {
         getDriver().switchTo().frame(getElement(locator));
     }
@@ -294,15 +73,6 @@ public class BasePage extends PageObject {
         getDriver().switchTo().defaultContent();
     }
 
-    public void doubleClickToElement(String locator) {
-        action = new Actions(getDriver());
-        action.doubleClick(getElement(locator)).perform();
-    }
-
-    public void rightClickToElement(String locator) {
-        action = new Actions(getDriver());
-        action.contextClick(getElement(locator)).perform();
-    }
 
     public void hoverMouseToElement(String locator) {
         action = new Actions(getDriver());
@@ -357,7 +127,6 @@ public class BasePage extends PageObject {
         String originalStyle = element.getAttribute("style");
         jsExecutor.executeScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, "style",
                 "border: 2px solid red; border-style: dashed;");
-        sleepInSecond(1);
         jsExecutor.executeScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, "style",
                 originalStyle);
     }
@@ -400,41 +169,4 @@ public class BasePage extends PageObject {
         }
     }
 
-    public void waitToElementVisible(String locator) {
-        explicitWait = new WebDriverWait(getDriver(), GlobalConstants.LONG_TIMEOUT);
-        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(getByXpath(locator)));
-    }
-
-    public void waitToElementInvisible(String locator) {
-        explicitWait = new WebDriverWait(getDriver(), GlobalConstants.SHORT_TIMEOUT);
-        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(getByXpath(locator)));
-    }
-
-    public void waitToElementVisible(String locator, String... values) {
-        explicitWait = new WebDriverWait(getDriver(), GlobalConstants.LONG_TIMEOUT);
-        explicitWait
-                .until(ExpectedConditions.visibilityOfElementLocated(getByXpath(getDynamicLocator(locator, values))));
-    }
-
-    public void waitToElementInvisible(String locator, String... values) {
-        explicitWait = new WebDriverWait(getDriver(), GlobalConstants.LONG_TIMEOUT);
-        explicitWait
-                .until(ExpectedConditions.invisibilityOfElementLocated(getByXpath(getDynamicLocator(locator, values))));
-    }
-
-
-    public String getDynamicLocator(String locator, String... values) {
-        locator = String.format(locator, (Object[]) values);
-        return locator;
-    }
-
-    public void waitToElementClickable(String locator, String... values) {
-        explicitWait = new WebDriverWait(getDriver(), GlobalConstants.LONG_TIMEOUT);
-        explicitWait.until(ExpectedConditions.elementToBeClickable(getByXpath(getDynamicLocator(locator, values))));
-    }
-
-    public void waitAlertPresence() {
-        explicitWait = new WebDriverWait(getDriver(), GlobalConstants.LONG_TIMEOUT);
-        explicitWait.until(ExpectedConditions.alertIsPresent());
-    }
 }
